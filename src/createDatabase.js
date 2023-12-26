a19c21c5ad2961b54f9fb8ea92573654120e68b9
ymvk361 <msmohitsharma546@gmail.com>
2023-12-26 11:49:03 +0530

    coding of datafiles

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,1 +1,44 @@
-// create database file
+// Import necessary modules and models
+const mongoose = require("mongoose");
+const Subscriber = require("./models/subscriber");
+const data = require("./data");
+//-----------------------------
+require("dotenv").config();
+
+// Set up the database connection URL from environment variables
+const dbUrl = process.env.MONGODB_URI;
+
+// Connect to the MongoDB database
+mongoose
+    .connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database connected"); /*once database connected then should show "Database connected" */
+    })
+    .catch((err) => {
+        console.log("Error connecting to the database", err);
+    });
+
+// Refresh data in the subscribers collection
+async function refreshAllData() {
+    try {
+        // Delete all existing documents in the subscribers collection
+        await Subscriber.deleteMany({}, { wtimeout: 30000 });
+        console.log("Deleted all subscribers");
+
+        // Insert new subscriber data into the subscribers collection
+        const newSubscribers = await Subscriber.insertMany(data);
+        console.log(`Added ${newSubscribers.length} new subscribers`);
+    } catch (err) {
+        console.log("Error refreshing data", err);
+    } finally {
+        // Disconnect from the database after data refresh
+        mongoose.disconnect();
+        console.log("Disconnected from the database");
+    }
+}
+
+// Call the function to refresh data in the subscribers collection
+refreshAllData();
--- src/data.js
+++ src/data.js
@@ -1,1 +1,18 @@
-// data file
+// Sample data module containing an array of subscriber objects with name and subscribedChannel properties
+
+const data = [
+    {
+      "name": "Jeread Krus",
+      "subscribedChannel": "CNET"
+    },
+    {
+      "name": "John Doe",
+      "subscribedChannel": "freeCodeCamp.org"
+    },
+    {
+      "name": "Lucifer",
+      "subscribedChannel": "Sentex"
+    }
+  ]
+
+module.exports = data;
