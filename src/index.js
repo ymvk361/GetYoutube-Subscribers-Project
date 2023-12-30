d076ef765cc7efc1b4ec6f48a00439d1be771dfc
ymvk361 <msmohitsharma546@gmail.com>
2023-12-30 22:10:56 +0530

    Make HTML File and modify database

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ app.use(express.urlencoded({extended: false}));
 const dbUrl = process.env.MONGODB_URI;
 mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result)=>{
-    console.log("connected to database");
-    app.listen(port, () => console.log(`App listening on port ${port}`));
+    console.log("Database Connected Successfully - All Ok");
+    app.listen(port, () => console.log(`Application is listening on Local Port ${port}`));
 })
 .catch((err)=>{
     console.log(error);
